4158a213e088053b38e09044fc78e603f99f3a98
Federico Tersigni <FederTersig@gmail.com>
2017-01-26 00:29:21 +0100

    Update SalvaIndirizzo.js

--- App/www/js/views/pages/SalvaIndirizzo.js
+++ App/www/js/views/pages/SalvaIndirizzo.js
@@ -1,1 +1,28 @@
 /*File js di salvaIndirizzo*/
+define(function(require){
+	var $ = require("jquery");
+    var Backbone = require("backbone");
+    var Utils = require("utils");
+    var handlebars = require("handlebars");
+    console.log("Entro in SalvaIndirizzo");
+
+    var salvaIndirizzo = Utils.Page.extend({
+    	constructorName: "SalvaIndirizzo",
+    	initialize: function(){
+    		this.template=Utils.templates.salvaIndirizzo;
+    	},
+
+    	id:"",
+
+    	className:"",
+
+    	events: {},
+
+    	render: function(){
+    		this.el.innerHTML=this.template({});
+    		return this;
+    	},
+
+    });
+    return salvaIndirizzo;
+});
