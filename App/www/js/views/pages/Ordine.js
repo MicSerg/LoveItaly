d2f7a2a7c472d80db121b64f1cba764f9d0e29f1
Federico Tersigni <FederTersig@gmail.com>
2017-01-26 00:28:27 +0100

    Update Ordine.js

--- App/www/js/views/pages/Ordine.js
+++ App/www/js/views/pages/Ordine.js
@@ -1,1 +1,28 @@
 /*Codice di ordine.js*/
+define(function(require){
+	var $ = require("jquery");
+    var Backbone = require("backbone");
+    var Utils = require("utils");
+    var handlebars = require("handlebars");
+    console.log("Entro in Ordine");
+
+    var ordine = Utils.Page.extend({
+    	constructorName: "Ordine",
+    	initialize: function(){
+    		this.template=Utils.templates.ordine;
+    	},
+
+    	id:"",
+
+    	className:"",
+
+    	events: {},
+
+    	render: function(){
+    		this.el.innerHTML=this.template({});
+    		return this;
+    	},
+
+    });
+    return ordine;
+});
