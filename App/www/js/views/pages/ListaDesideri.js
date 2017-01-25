42eb78c75a5fa4cffbe6d2991e54bba1b3fb92d5
Federico Tersigni <FederTersig@gmail.com>
2017-01-26 00:30:14 +0100

    Update ListaDesideri.js

--- App/www/js/views/pages/ListaDesideri.js
+++ App/www/js/views/pages/ListaDesideri.js
@@ -1,1 +1,28 @@
 /*Codice js ListaDesideri*/
+define(function(require){
+	var $ = require("jquery");
+    var Backbone = require("backbone");
+    var Utils = require("utils");
+    var handlebars = require("handlebars");
+    console.log("Entro in ListaDesideri");
+
+    var ListaDesideri = Utils.Page.extend({
+    	constructorName: "ListaDesideri",
+    	initialize: function(){
+    		this.template=Utils.templates.listaDesideri;
+    	},
+
+    	id:"",
+
+    	className:"",
+
+    	events: {},
+
+    	render: function(){
+    		this.el.innerHTML=this.template({});
+    		return this;
+    	},
+
+    });
+    return listaDesideri;
+});
