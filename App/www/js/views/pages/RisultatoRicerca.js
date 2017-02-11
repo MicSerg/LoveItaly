00e4eb2b44f4a220b87e2bea7af33bb4b15106c6
Federico Tersigni <FederTersig@gmail.com>
2017-02-11 23:02:26 +0100

    update
    Implementazione di risultato ricerca con modello, ma c'è qualcosa che dà
    errore.

--- App/www/js/views/pages/RisultatoRicerca.js
+++ App/www/js/views/pages/RisultatoRicerca.js
@@ -1,9 +1,11 @@
 define(function(require){
     var $ = require("jquery");
     var Backbone = require("backbone");
-    var Lista_Ricerca = require("models/SearchList");
-    
     var Utils = require("utils");
+    var Lista_Ricerca = require("models/SearchList");
+    var Prodotti = require("models/Prodotti");
+    //var detProdotto = require("views/pages/dettaglioProdotto");
+
 
     var risultatoRicerca = Utils.Page.extend({
 
@@ -11,11 +13,11 @@ define(function(require){
 
         model: Lista_Ricerca,
 
-        initialize: function(){
+        initialize: function(variabile){
             this.template = Utils.templates.risultatoricerca;
         },
 
-        id: "",
+        id: "r_ricerca",
         className: "",
 
         events: {
@@ -23,11 +25,38 @@ define(function(require){
         },
 
         render: function(){
-            this.el.innerHTML = this.template({});
-            return this;
+            var temp = localStorage.getItem("datoricerca");
+            var model = new Lista_Ricerca({
+                id: temp
+            });
+
+            var that = this;
+            model.fetch({
+                success: function(){
+                    var temp= model.toJSON();
+                    
+                    for (var i = 0; i < (Object.keys(temp).length)-1; i++) {
+                        var idprod = ((model.toJSON())[i]).id;
+                        var idtemp= model.toJSON()[i];
+                        idimg = (idtemp.associations.images[0]).id;
+                        idprod = idprod;
+                        var img2 = 'http://192.168.56.101/loveitaly/api/images/products/' + idprod + '/' + idimg + '/?ws_key=IYI6M35MLB8UVW38Y99RY3YPQWRX5X8H';
+                        ((model.toJSON())[i]).img= img2;
+                        ((model.toJSON())[i]).price= parseFloat(((model.toJSON())[i]).price).toFixed(2);
+                    }
+                    
+                    $(that.el).html(that.template((model.toJSON())));
+                    return that;
+
+                }
+            });
         },
 
-        toDettProdotto: function(event){
+        toDettProdotto: function(e){
+            e.preventDefault();
+            var datoprod=$(e.currentTarget).attr("data-prod");
+            localStorage.setItem("datoprod", datoprod);
+
             Backbone.history.navigate("dettaglioprodotto",{
                 trigger: true
             });
