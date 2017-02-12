55fa0e0a9f670526803affcf1935b111c79c6e05
MicSerg <michele.sergola@student.univaq.it>
2017-02-12 10:48:52 +0100

    Update RisultatoCategoria.js

--- App/www/js/views/pages/RisultatoCategoria.js
+++ App/www/js/views/pages/RisultatoCategoria.js
@@ -2,6 +2,8 @@ define(function(require){
 	var $ = require("jquery");
 	var Backbone = require("backbone");
 	var Lista_Prod_Cat = require("models/ListaProdottiDaCategoria");
+    var Prodotti = require ("models/Prodotti");
+    var VistaProdotti = require ("views/pages/VistaProdotti");
 	
     var Utils = require("utils");
 
@@ -23,8 +25,39 @@ define(function(require){
     	},
 
     	render: function(){
-    		this.el.innerHTML = this.template({});
-    		return this;
+    		var temp = localStorage.getItem("datocategoria");
+            var model = new Lista_Prod_Cat({
+                id: temp
+            });
+
+            var that = this;
+            model.fetch({
+                success: function() {
+                    var temp= model.toJSON();
+
+
+                    /*****************************************************
+                     * Riempio gli oggetti dell'array con le immagini
+                     * in modo da usufruirne nella lista, mostrandole
+                     * all'utente
+                     *****************************************************/
+
+                    for (var i = 0; i < (Object.keys(temp).length)-1; i++) {
+
+                        var idprod = ((model.toJSON())[i]).id;
+                        var idtemp= model.toJSON()[i];
+                        idimg = (idtemp.associations.images[0]).id;
+                        idprod = idprod;
+                        var img2 = 'http://192.168.56.101/loveitaly/api/images/products/' + idprod + '/' + idimg + '/?ws_key=IYI6M35MLB8UVW38Y99RY3YPQWRX5X8H';
+                        ((model.toJSON())[i]).img= img2;
+                        ((model.toJSON())[i]).price= parseFloat(((model.toJSON())[i]).price).toFixed(2);
+                    }
+
+
+                    $(that.el).html(that.template((model.toJSON())));
+                    return that;
+                }
+            });
     	},
 
         toDettProdotto: function(event){
