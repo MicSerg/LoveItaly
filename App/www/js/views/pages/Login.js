051a9e4268ff50cb79e617cd94e95502f57c25d0
MicSerg <michele.sergola@student.univaq.it>
2017-02-13 23:08:23 +0100

    Update Login.js

--- App/www/js/views/pages/Login.js
+++ App/www/js/views/pages/Login.js
@@ -12,19 +12,16 @@ define(function(require){
     	model: User,
 
     	initialize: function(){
-            console.log("ECCO IL LOGIN");
-            console.log(this.structureView);
+            
     		this.template = Utils.templates.login;
-            console.log(this.template);
+            
     	},
 
     	id: "",
     	className: "",
         switchDes: false,
         menuView: "insdatispedizione",
-        /* in events in pratica scriviamo: "tipodievento   tagHTMLinteressatoAquelevento :
-        qualeFunzionedevievocare -> (La funzione che viene evocata per ora è headNavig che
-        è dichiarata dopo la funzione "render". */
+        
     	events: {
     		"click #buttonLogin" : "doLogin"
     	},
@@ -37,18 +34,9 @@ define(function(require){
     		return this;
     	},
 
-        /* 
-        http://192.168.56.101/loveitaly/admin1/
-
-        studente@mobile.it
-        studente
-
-        per fare un ottimo login bisogna usare
-        nick: miao@miao.miao
-        pass: miaomiao
-        */
+        
         doLogin: function(event){
-            /*COMINCIANO MODIFICHE*/
+            
 
             el: $("lForm");
             var self=this;
@@ -69,15 +57,15 @@ define(function(require){
                         
 
                     if(self.switchDes){
-                            //vai in insDatiSpedizione
-                            console.log("da LOGIN vado in spedizione");
+                            
+                            
                             Backbone.history.navigate("showheadmenu",{
                                 trigger: true
                             });
                     }else{
-                            console.log("da LOGIN vado in headNavig");
-                            //MANCA THIS.SECONDVIEW PER
-                            //INDIRIZZARE NEL PRECISO POSTO LASCIATO!!!!!
+                            
+                            
+                            
                             Backbone.history.navigate("showheadnavig",{
                                 trigger: true
                             });
@@ -86,16 +74,12 @@ define(function(require){
                     
                 },
                 error: function(){
-                    //condizione di errore
+                    
                     
                 }
             });
 
-            /*FINISCONO MODIFICHE*/
-            /*
-            Backbone.history.navigate("showheadnavig",{
-                trigger: true
-            });*/
+            
         }
 
     });
